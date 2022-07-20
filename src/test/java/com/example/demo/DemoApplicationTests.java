7575d497bbe98b48940a9efb093cb54a5706cedc
jeevana6 <46052882+jeevana6@users.noreply.github.com>
2022-07-20 22:46:45 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests1 {
+class DemoApplicationTests11 {
 
 	@Test
 	void contextLoads() {
