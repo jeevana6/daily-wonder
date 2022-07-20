ca0435b6645a4b17d172d06a42c9a9302f07fe04
jeevana6 <jeevana1669@gmail.com>
2022-07-20 22:50:51 +0530

    2nd commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests11 {
+class DemoApplicationTests12 {
 
 	@Test
 	void contextLoads() {
